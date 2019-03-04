03d838abdefe704b778deb9a77ff7ab2a94a9f77
smarks8 <staceyamarks@gmail.com>
2019-03-04 01:03:55 -0500

    fix button link

--- src/BookCard.js
+++ src/BookCard.js
@@ -9,7 +9,7 @@ const BookCard = (props) => {
                 <h2>{props.title}</h2>
                 <h3>{props.author}</h3>
                 <h3>{props.publisher}</h3>
-                <button className="card-button"> <a className="card-link" target="_blank" href={props.externalLink}>MORE ABOUT ME</a></button>
+                <a className="card-link" target="_blank" href={props.externalLink}><button className="card-button"> MORE ABOUT ME</button></a>
             </div>
         </div>
     )
